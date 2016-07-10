84fab0c1fdfa0d1b199fbb2d95260ba7aa3adb18
crtnywllr <courtneycmeadows@gmail.com>
2016-07-09 19:18:30 -0600

    JS refactor

--- js/script.js
+++ js/script.js
@@ -1,73 +1,52 @@
 $(document).ready(function () {
 
+    function showHide(hide, show, showImage) {
+        hide.forEach(function (el) {
+            el.toggle();
+            el.addClass('animated fadeIn');
+        });
+        $("i").toggleClass('fa-chevron-down fa-chevron-up');
+        $('.description').toggle();
+        $('.contract').toggle();
+        show.addClass('animated fadeIn');
+        show.toggleClass('box shortBox');
+        show.toggleClass('col-md-3 col-md-12');
+        showImage.toggle();
+        showImage.addClass('animated fadeIn bottomSpacer')
+    };
+
     //Expand and Contract Works
-    $('#pictionary').on('click', function () {
-        $('#pictionary').toggleClass('box shortBox')
-        $('#pictionary').toggleClass('col-md-3 col-md-12');
-        $("i").toggleClass('fa-chevron-down fa-chevron-up');
-        $('#quizApp').toggle();
-        $('#apiHack').toggle();
-        $('#noteKeeper').toggle();
-        $('.description').toggle();
-        $('.contract').toggle();
-        $('#pictionaryImage').toggle();
-        $('#pictionaryImage').addClass('animated fadeIn bottomSpacer');
-    })
-    $('#quizApp').on('click', function () {
-        $('#quizApp').toggleClass('box shortBox')
-        $('#quizApp').toggleClass('col-md-3 col-md-12');
-        $("i").toggleClass('fa-chevron-down fa-chevron-up');
-        $('#pictionary').toggle();
-        $('#apiHack').toggle();
-        $('#noteKeeper').toggle();
-        $('.description').toggle();
-        $('.contract').toggle();
-        $('#quizAppImage').toggle();
-        $('#quizAppImage').addClass('animated fadeIn bottomSpacer');
-    })
+
     $('#apiHack').on('click', function () {
-        $('#apiHack').toggleClass('box shortBox')
-        $('#apiHack').toggleClass('col-md-3 col-md-12');
-        $("i").toggleClass('fa-chevron-down fa-chevron-up');
-        $('#quizApp').toggle();
-        $('#pictionary').toggle();
-        $('#noteKeeper').toggle();
-        $('.description').toggle();
-        $('.contract').toggle();
-        $('#apiHackImage').toggle();
-        $('#apiHackImage').addClass('animated fadeIn bottomSpacer');
-    })
+        showHide([$('#quizApp'), $('#pictionary'), $('#noteKeeper')], $('#apiHack'), $('#apiHackImage'));
+    });
+    $('#quizApp').on('click', function () {
+        showHide([$('#pictionary'), $('#apiHack'), $('#noteKeeper')], $('#quizApp'), $('#quizAppImage'));
+    });
+    $('#pictionary').on('click', function () {
+        showHide([$('#quizApp'), $('#apiHack'), $('#noteKeeper')], $("#pictionary"), $("#pictionaryImage"));
+    });
     $('#noteKeeper').on('click', function () {
-        $('#noteKeeper').toggleClass('box shortBox')
-        $('#noteKeeper').toggleClass('col-md-3 col-md-12');
-        $("i").toggleClass('fa-chevron-down fa-chevron-up');
-        $('#quizApp').toggle();
-        $('#apiHack').toggle();
-        $('#pictionary').toggle();
-        $('.description').toggle();
-        $('.contract').toggle();
-        $('#noteKeeperImage').toggle();
-        $('#noteKeeperImage').addClass('animated fadeIn bottomSpacer');
-    })
+        showHide([$('#quizApp'), $('#apiHack'), $('#pictionary')], $('#noteKeeper'), $('#noteKeeperImage'));
+    });
 
 
     //Setting Scrolls
     $(".navbar-brand").click(function () {
         $(".hero").velocity("scroll", 1000);
-    })
-
+    });
     $("#toProfile").click(function () {
         $("#intro").velocity("scroll", 1000);
-    })
+    });
     $("#profileLink").click(function () {
         $("#intro").velocity("scroll", 1000);
-    })
+    });
     $("#portfolioLink").click(function () {
         $("#works").velocity("scroll", 1000);
-    })
+    });
     $("#contactLink").click(function () {
         $("#contact").velocity("scroll", 1000);
-    })
+    });
 
     //Callout Styles
     $("#toProfile").velocity({
